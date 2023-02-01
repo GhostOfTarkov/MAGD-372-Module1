f8ba941a9ca8007bffdac9d479eb95957cdab580
GhostOfTarkov <mattwalt10@gmail.com>
2023-02-01 15:30:07 -0600

    Initial Project

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Instantiate(cubePrefab, transform position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+    public GameObject objectToPool;
+
+    public List<GameObject> thePool = new List<GameObject>();
+
+    public int startAmount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
